b34d87872d7dfe2275c0c4c4a67733c2b8dd2443
HaiBanh0105 <vodathai91thcsduclap@gmail.com>
2025-04-06 16:05:16 +0700

    change

--- GUI/frmDinhLuong.cs
+++ GUI/frmDinhLuong.cs
@@ -14,25 +14,28 @@ namespace GUI
     public partial class frmDinhLuong : Form
     {
         BUS_DinhLuong dinhluong;
-
-        public frmDinhLuong()
+        string Masp;
+        public frmDinhLuong(string masp, string tensp)   //truyền mã sản phẩm và tên sản phẩm vào form định lượng
         {
             InitializeComponent();
+            txtTenSp.Text = tensp;  
+            txtTenSp.ReadOnly = true;
+            Masp = masp;
         }
 
         //Tải tên sản phẩm lên combobox
-        public void TaitenSp()
-        {
-            dinhluong = new BUS_DinhLuong("", "", 0);
-            cboTenSp.DataSource = dinhluong.TaiTenSp();
-            cboTenSp.DisplayMember = "TenSp";
-        }
+        //public void TaitenSp()
+        //{
+        //    dinhluong = new BUS_DinhLuong("", "", 0);
+        //    cboTenSp.DataSource = dinhluong.TaiTenSp();
+        //    cboTenSp.DisplayMember = "TenSp";
+        //}
 
         //Load lại danh sách định lượng theo tên sản phẩm trên griview
         public void loadDsDinhluong()
         {
             dinhluong = new BUS_DinhLuong("", "", 0);
-            gridDsDinhluong.DataSource = dinhluong.TaiDsDinhluong(cboTenSp.Text);
+            gridDsDinhluong.DataSource = dinhluong.TaiDsDinhluong(txtTenSp.Text);
 
         }
 
@@ -40,14 +43,13 @@ namespace GUI
         private void frmDinhLuong_Load(object sender, EventArgs e)
         {
             loadDsDinhluong();
-            TaitenSp();
         }
 
         //Nút thêm để thêm định lượng vào database
         private void btnThem_Click(object sender, EventArgs e)
         {
             dinhluong = new BUS_DinhLuong("", "", 0);
-            dinhluong.ThemDinhluong(cboTenNguyenLieu.Text, cboTenNguyenLieu.Text, (int)numSoluongNL.Value);
+            dinhluong.ThemDinhluong(Masp, cboTenNguyenLieu.Text, (int)numSoluongNL.Value);
 
             numSoluongNL.Value = 0;
             loadDsDinhluong();
